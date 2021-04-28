fab2a4f3c6ca5764ad03e86086381efef31bdbc5
hazim0001 <hazim_alsayed@hotmail.com>
2021-04-28 04:17:03 +0200

    init commit

---
+++ src/components/common/like.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+//input: liked booolean
+// output: onClick
+
+class Like extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Like;
---
+++ src/components/movies.jsx
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import {
+  getMovies,
+  deleteMovie,
+} from "../Starter Code/services/fakeMovieService";
+
+class Movies extends Component {
+  constructor() {
+    super();
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleLike = this.handleLike.bind(this);
+  }
+  state = {
+    movies: getMovies(),
+  };
+
+  addLiked() {
+    getMovies().map((movie) => (movie.liked = false));
+  }
+
+  componentDidMount() {
+    this.addLiked();
+  }
+
+  handleDelete(id) {
+    deleteMovie(id._id);
+    this.setState({ movies: getMovies() });
+  }
+
+  handleLike(movie) {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movie };
+    movies[index].movie.liked = !movies[index].movie.liked;
+    this.setState({ movies });
+  }
+
+  moviesList() {
+    // console.log(this.state.movies);
+    return this.state.movies.map((movie) => (
+      <tr key={movie._id}>
+        <th>{movie.title}</th>
+        <td>{movie.genre.name}</td>
+        <td>{movie.numberInStock}</td>
+        <td>{movie.dailyRentalRate}</td>
+        <td onClick={() => this.handleLike({ movie })}>
+          {movie.liked ? (
+            <i className="fa fa-heart"></i>
+          ) : (
+            <i className="fa fa-heart-o"></i>
+          )}
+        </td>
+        <td>
+          <button
+            className="btn btn-danger"
+            onClick={() => this.handleDelete({ _id: movie._id })}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    ));
+  }
+
+  conditionalMovies() {
+    // console.log(this.state);
+    if (this.state.movies.length === 0)
+      return <h3>There are no movies in the database</h3>;
+    return (
+      <div>
+        <h5>Showing {this.state.movies.length} movies in the database</h5>
+        <table className="table mt-3">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>{this.moviesList()}</tbody>
+        </table>
+      </div>
+    );
+  }
+
+  render() {
+    // console.log(this.state.movies);
+    return <main className="container mt-5">{this.conditionalMovies()}</main>;
+  }
+}
+
+export default Movies;
